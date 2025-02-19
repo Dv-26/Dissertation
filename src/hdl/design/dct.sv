1c4e1d9b9da3092188ec3d71ee4cb6080e4925ee
DV_26 <1060833436@qq.com>
2025-02-19 08:51:48 +0800

    changed src

--- src/hdl/design/dct.sv
+++ src/hdl/design/dct.sv
@@ -1,63 +1,49 @@
-module Pe
-#(
+typedef struct {
+  logic [DATA_WIDTH-1:0] x;
+  logic [DATA_WIDTH-1:0] coefficient;
+  logic [DATA_WIDTH-1:0] z;
+  logic sumDiffSel;
+  logic load;
+} peBus_t #(parameter DATA_WIDTH = 8);
+
+
+module Pe #(
   parameter DATA_WIDTH = 8
 )(
   input   logic                   clk,
   input   logic                   rst_n,
 
-  input   logic [DATA_WIDTH-1:0]  xIn,
-  input   logic [DATA_WIDTH-1:0]  xDelayIn,
-  input   logic [DATA_WIDTH-1:0]  zIn,
-  input   logic [DATA_WIDTH-1:0]  CoefficientIn,
-
-  output  logic [DATA_WIDTH-1:0]  xOut,
-  output  logic [DATA_WIDTH-1:0]  xDelayOut,
-  output  logic [DATA_WIDTH-1:0]  zOut,
-  output  logic [DATA_WIDTH-1:0]  CoefficientOut,
-
-  input   logic                   sumDiffSelIn,
-  input   logic                   loadIn,
-
-  output  logic                   sumDiffSelOut,
-  output  logic                   loadOut
+  input   peBus_t #(DATA_WIDTH)   in,
+  output  peBus_t #(DATA_WIDTH)   out
 );
 
-  logic [DATA_WIDTH-1:0] xDelay;
-  always_ff @(posedge clk)
-    xDelay <= xIn;
+struct peBus_t #(DATA_WIDTH)  inDealy[2];
+always_ff @(posedge clk)begin
+  inDealy[0] <= in;
+  inDealy[1] <= inDealy[0]
+end
 
-  logic [DATA_WIDTH-1:0] sum, diff;
-  assign sum = xIn + xDelay;
-  assign diff = xDelay - xDelayIn;
+assign out.x = inDealy[1].x;
+assign out.sumDiffSel = inDealy[1].sumDiffSel;
+assign out.load = inDealy[1].load;
 
-  logic [DATA_WIDTH-1:0]  product;
-  logic sumDiffSel;
-  always_ff @(posedge clk)
-    sumDiffSel <= sumDiffSelIn;
-  
-  assign product = CoefficientIn * (sumDiffSel? diff : sum);
+logic [DATA_WIDTH-1:0]  sum,diff;
+assign sum = in.x + inDealy[0].x;
+assign diff = inDealy[0].x - inDealy[1].x
 
-  logic [DATA_WIDTH-1:0] DelayIn, DelayOut, accumulate;
-  logic load;
-  assign accumulate = product + DelayOut;
-  always_ff @(posedge clk)
-    load <= loadIn;
-  assign DelayIn = load? product : accumulate;
-  Delay #(DATA_WIDTH, 2)Delay_2(clk, DelayIn, DelayOut); 
+logic [DATA_WIDTH-1:0]  product;
+assign product = in.coefficient * (inDealy[0].sumDiffSel? sum : diff);
 
-  logic outSel;
-  Delay #(1, 6)Delay_7(clk, load, outSel); 
+logic [DATA_WIDTH-1:0]  delay2In,delay2Out;
+Delay #(DATA_WIDTH, 2)delay2(clk, delay2In, delay2Out);
 
-  always_ff @(posedge clk)
-    CoefficientOut <= CoefficientIn;
+logic [DATA_WIDTH-1:0] acc;
+assign acc = product + delay2Out;
+assing delay2In = inDealy[0].load? product : acc;
 
-
-  assign sumDiffSelOut = sumDiffSel;
-  assign loadOut = load;
-  assign zOut = outSel? accumulate : zIn;
-  assign xOut = xDelay;
-  assign xDelayOut = xDelay;
-
+logic outSel; 
+Delay #(1, 6)delay6(clk, inDealy.load[0], outSel); 
+assign out.z = outSel? acc : in.z;
 endmodule
 
 
