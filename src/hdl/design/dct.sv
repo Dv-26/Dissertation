005fdaa86e644326fa23d85c049d30ded3c7c4c0
DV_26 <1060833436@qq.com>
2025-02-24 02:07:21 +0800

    add ctrl logic

--- src/hdl/design/dct.sv
+++ src/hdl/design/dct.sv
@@ -6,29 +6,54 @@ module dct #(
   input logic clk,
   input logic rst_n,
 
-  input logic [DATA_WIDTH-1:0] x,
-  input logic sumDiffSel,
-  input logic load,
+//  input logic [DATA_WIDTH-1:0] x,
+//  input logic sumDiffSel,
+//  input logic load,
+  input x2zZ_t x,
 
-  output logic [DATA_WIDTH-1:0] y,
-  output logic valid
+  output z2yY_t y
 );
 
   logic [DATA_WIDTH-1:0] coefficient[4];
 
   assign coefficient = {1, 1, 1, 1};
-  logic [DATA_WIDTH-1:0] z;
-  logic zValid;
-  x2zArray #(DATA_WIDTH, 4) x2z (clk, rst_n, coefficient, x, sumDiffSel, load, z, zValid);
+ // logic [DATA_WIDTH-1:0] z;
+  x2zZ_t z;
+  x2zArray #(DATA_WIDTH, 4) x2z (clk, rst_n, coefficient, x, z);
+
+  logic [3:0] cnt16;
+  always_ff @(posedge clk)begin
+    if(rst_n) begin
+      cnt16 <= 0;
+    end else begin
+      if(z.valid)
+        cnt16 <= cnt16 + 1;
+    end
+  end
 
   logic sel;
-  logic [DATA_WIDTH-1:0] delay8In, delay8Out, sumDiff;
-  Delay #(DATA_WIDTH, 8)delay8(clk, delay8In, delay8Out);
-  assign delay8In = sel? z : z-delay8Out;
-  assign sumDiff = sel? delay8Out : delay8Out+z;
+  z2yZ_t sumDiff;
+  logic [DATA_WIDTH-1:0] delay8In, delay8Out;
+  Delay #(DATA_WIDTH+1, 8)delay8(clk, delay8In, delay8Out);
+  assign delay8In = sel? z.data-delay8Out : z.data;
+  assign sumDiff.data = sel? delay8Out+z : delay8Out;
+  assign sel = cnt[3] & z.valid;
 
-  logic zLoad;
-  z2yArray #(DATA_WIDTH, 4) z2y (clk, rst_n, coefficient, sumDiff, load, y, valid);
+  logic sumDiffValid;
+  Delay #(1, 8)validDelay(clk, z.valid, sumDiffValid);
+
+  logic [5:0] cnt64;
+  always_ff @(posedge clk)begin
+    if(rst_n) begin
+      cnt64 <= 0;
+    end else begin
+      if(zValidDelay)
+        cnt64 <= cnt64 + 1;
+    end
+  end
+
+  assign sumDiff.load = ~|cnt64[5:4] & sumDiffValid;
+  z2yArray #(DATA_WIDTH, 4) z2y (clk, rst_n, coefficient, sumDiff, y);
 endmodule
 
 module x2zArray #(
@@ -40,18 +65,20 @@ module x2zArray #(
 
   input logic [DATA_WIDTH-1:0]  coefficient[COL],
 
-  input logic [DATA_WIDTH-1:0] x,
-  input logic sumDiffSel,
-  input logic load,
-
-  output logic [DATA_WIDTH-1:0] z,
-  output logic valid
+  input x2zX_t x,
+//  input logic [DATA_WIDTH-1:0] x,
+//  input logic sumDiffSel,
+//  input logic load,
+  output x2zZ_t z
+//  output logic [DATA_WIDTH-1:0] z,
+//  output logic valid
 );
 
 // verilator lint_off UNOPTFLAT
   x2zPort_t rowPort[COL+1]; 
 // verilator lint_on UNOPTFLAT
-  assign {rowPort[0].x.data, rowPort[0].x.sumDiffSel, rowPort[0].x.load} = {x, sumDiffSel, load}; 
+//  assign {rowPort[0].x.data, rowPort[0].x.sumDiffSel, rowPort[0].x.load} = {x, sumDiffSel, load}; 
+  assign rowPort[0].x = x;
   assign {rowPort[0].z.data, rowPort[0].z.valid} = '0;
 
   generate
@@ -84,28 +111,27 @@ module x2zArray #(
     end
   endgenerate
 
-  assign z = rowPort[COL].z.data;
-  assign valid = rowPort[COL].z.valid;
+//  assign z = rowPort[COL].z.data;
+//  assign valid = rowPort[COL].z.valid;
+    assign z = rowPort[COL].z;
 endmodule
 
 module z2yArray #(
   parameter DATA_WIDTH = 8,
   parameter COL = 4
 ) (
-  input wire  clk,
-  input wire  rst_n,
-  input logic [DATA_WIDTH-1:0]  coefficient[COL],
+  input   wire  clk,
+  input   wire  rst_n,
+  input   logic [DATA_WIDTH-1:0]  coefficient[COL],
 
-  input logic [DATA_WIDTH-1:0] z,
-  input logic load,
-
-  output logic [DATA_WIDTH-1:0] y,
-  output logic valid
+  input   z2yZ_t z,
+  output  z2yY_t y,
 );
 // verilator lint_off UNOPTFLAT
   z2yPort_t rowPort[COL+1];
 // verilator lint_on UNOPTFLAT
-  assign {rowPort[0].z.data, rowPort[0].z.load} = {z, load};
+//  assign {rowPort[0].z.data, rowPort[0].z.load} = {z, load};
+  assign rowPort[0].z = z;
   assign {rowPort[0].y.data, rowPort[0].y.valid} = '0;
   generate 
     genvar i;
@@ -129,6 +155,7 @@ module z2yArray #(
     end
   endgenerate
 
-  assign {y, valid} = {rowPort[COL].y.data, rowPort[COL].y.valid};
+//  assign {y, valid} = {rowPort[COL].y.data, rowPort[COL].y.valid};
+  assign y = rowPort[COL].y;
 
 endmodule
