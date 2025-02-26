aa93e09d692465f803a0ebd07cefd8167b3b6374
huangzhiwei <huangzhiwei26@foxmail.com>
2025-02-26 15:31:04 +0800

    add coefficient

---
+++ src/dct.py
@@ -0,0 +1,51 @@
+import math
+import numpy as np
+
+def C(x):
+    return 1 / math.sqrt(2) if x==0 else 1
+
+X = np.array([[0, 2, 4, 6, 7, 5, 3, 1] for _ in range(8)])
+
+def compute_Y(u, v):
+    sum_result = 0
+    for i in range(8):
+        for j in range(8):
+            sum_result += X[i, j] * math.cos((2 * i + 1) * u * math.pi / 16) * math.cos((2 * j + 1) * v * math.pi / 16)
+    return (1 / 4) * C(u) * C(v) * sum_result
+
+Y0 = np.zeros((8, 8))
+for u in range(8):
+    for v in range(8):
+        Y0[u, v] = compute_Y(u, v)
+Y0Rounded = np.round(Y0, 2)
+
+pi = np.pi
+
+# 计算 a, b, c, d, e, f, g
+a = 0.5 * np.cos(4 * pi / 16)
+b = 0.5 * np.cos(1 * pi / 16)
+c = 0.5 * np.cos(2 * pi / 16)
+d = 0.5 * np.cos(3 * pi / 16)
+e = 0.5 * np.cos(4 * pi / 16)
+f = 0.5 * np.cos(5 * pi / 16)
+g = 0.5 * np.cos(6 * pi / 16)
+
+# 定义矩阵 C
+C = np.array([
+    [a, a, a, a, a, a, a, a],
+    [b, d, e, g, -g, -e, -d, -b],
+    [c, f, -f, -c, -c, -f, f, c],
+    [d, -g, -b, -e, e, b, g, -d],
+    [a, -a, -a, a, a, -a, -a, a],
+    [e, -b, g, d, -d, -g, b, -e],
+    [f, -c, c, -f, -f, c, -c, f],
+    [g, -e, d, -b, b, -d, e, -g]
+])
+
+Y1 =np.dot(C, X.T).T
+Y1Rounded = np.round(Y1, 2)
+print("Input matrix X:")
+print(X)
+print("\noutput matrix Y:")
+print(Y0Rounded)
+print(Y1Rounded)
--- src/hdl/design/dct.sv
+++ src/hdl/design/dct.sv
@@ -10,14 +10,10 @@ module dct #(
   output dctPort_t y
 );
 
-  logic [DATA_WIDTH-1:0] coefficient[4];
+  logic [DATA_WIDTH-1:0] x2zCoefficient[4];
 
   x2zX_t xIn;
   dctPort_t z;
-  assign coefficient[0] = 1;
-  assign coefficient[1] = 1;
-  assign coefficient[2] = 1;
-  assign coefficient[3] = 1;
  // logic [DATA_WIDTH-1:0] z;
   logic [2:0] cnt8;
   always_ff @(posedge clk or negedge rst_n)begin
@@ -31,7 +27,7 @@ module dct #(
   assign xIn.data = x.data;
   assign xIn.sumDiffSel = cnt8[0];
   assign xIn.load = ~|cnt8[2:1] & x.valid;
-  x2zArray #(DATA_WIDTH, 4) x2z (clk, rst_n, coefficient, xIn, z);
+  x2zArray #(DATA_WIDTH, 4) x2z (clk, rst_n, x2zCoefficient, xIn, z);
 
   logic [3:0] cnt16;
   always_ff @(posedge clk or negedge rst_n)begin
@@ -45,29 +41,52 @@ module dct #(
 
   logic sel;
   z2yZ_t sumDiff;
+  logic [DATA_WIDTH-1:0] z2yCoefficient[4];
   logic [DATA_WIDTH-1:0] delay8In, delay8Out;
   Delay #(DATA_WIDTH+1, 8)delay8(clk, rst_n, delay8In, delay8Out);
   assign delay8In = sel? z.data-delay8Out : z.data;
   assign sumDiff.data = sel? delay8Out+z.data : delay8Out;
   assign sel = cnt16[3] & z.valid;
 
-  logic sumDiffValid;
-  Delay #(1, 8)validDelay(clk, rst_n, z.valid, sumDiffValid);
+  logic sumDiffValidAhead, sumDiffValid;
+  Delay #(1, 7)validDelay7(clk, rst_n, z.valid, sumDiffValidAhead);
+  Delay #(1, 1)validDelay8(clk, rst_n, sumDiffValidAhead, sumDiffValid);
 
-  logic [5:0] cnt64;
+  logic [5:0] cnt64Ahead,cnt64;
   always_ff @(posedge clk or negedge rst_n)begin
     if(!rst_n) begin
-      cnt64 <= 0;
+      cnt64Ahead <= 0;
     end else begin
-      if(sumDiffValid)
-        cnt64 <= cnt64 + 1;
+      if(sumDiffValidAhead)
+        cnt64Ahead <= cnt64Ahead + 1;
     end
   end
+  Delay #(6, 1)cnt64Delay(clk, rst_n, cnt64Ahead, cnt64);
 
   assign sumDiff.load = ~|cnt64[5:4] & sumDiffValid;
-  z2yArray #(DATA_WIDTH, 4) z2y (clk, rst_n, coefficient, sumDiff, y);
+  z2yArray #(DATA_WIDTH, 4) z2y (clk, rst_n, z2yCoefficient, sumDiff, y);
+
+  coefficientMap #(DATA_WIDTH, 8) coefficientMap (
+    clk,
+    cnt8, x.valid, x2zCoefficient,
+    cnt64Ahead[5:3], sumDiffValidAhead, z2yCoefficient
+  );
 endmodule
 
+module multiplier #(
+  parameter   DATA_WIDTH = 8,
+  parameter   SHIFT = 8
+) (
+  input signed [DATA_WIDTH-1:0] coefficient,
+  input signed [DATA_WIDTH-1:0] in,
+  output signed [DATA_WIDTH-1:0]  out
+);
+
+logic signed [2*DATA_WIDTH-1 : 0] product;
+assign product = coefficient * in;
+assign out = product >>> SHIFT;
+
+endmodule
 module x2zArray #(
   parameter DATA_WIDTH = 8,
   parameter COL = 4
@@ -107,8 +126,11 @@ module x2zArray #(
       assign diff = xDelay2[0].data - xDelay2[1].data;
        
       logic [DATA_WIDTH-1:0] product;
-      assign product = coefficient[i] * (xDelay2[0].sumDiffSel? diff : sum);
-
+      multiplier#(DATA_WIDTH, DATA_WIDTH) multiplier (
+        coefficient[i],
+        xDelay2[0].sumDiffSel? diff : sum,
+        product
+      );
       logic [DATA_WIDTH-1 : 0] delay2In, delay2Out, acc;
       assign delay2In = xDelay2[0].load? product : acc;
       assign acc = product + delay2Out;
@@ -150,7 +172,11 @@ module z2yArray #(
       );
 
       logic [DATA_WIDTH-1 : 0] product, acc, delay16In, delay16Out;
-      assign product = coefficient[i] * rowPort[i].z.data;
+      multiplier#(DATA_WIDTH, DATA_WIDTH) multiplier (
+        coefficient[i],
+        rowPort[i].z.data,
+        product
+      );
       assign acc = product + delay16Out;
       Delay #(DATA_WIDTH, 16) delay16 (clk, rst_n, delay16In, delay16Out);
       assign delay16In = rowPort[i].z.load? product : acc;
@@ -164,4 +190,48 @@ module z2yArray #(
 
   assign y = rowPort[COL].y;
 
+endmodule
+
+module coefficientMap #(
+  parameter DATA_WIDTH = 8,
+  parameter DEPTH = 8
+) (
+  input logic clk,
+
+  input logic [$clog2(DEPTH)-1:0] addra,
+  input logic ena,
+  output logic [DATA_WIDTH-1:0] douta[4],
+
+  input logic [$clog2(DEPTH)-1:0] addrb,
+  input logic enb,
+  output logic [DATA_WIDTH-1:0] doutb[4]
+);
+
+  reg [DATA_WIDTH-1:0]  memoryArray[DEPTH][4];
+  always @(posedge clk)begin
+    if(ena)
+      douta <= memoryArray[addra];
+    if(enb)
+      doutb <= memoryArray[addrb];
+  end
+
+  localparam real PI = 3.14159265358979323846;  // Define π manually
+  logic signed [DATA_WIDTH-1:0] cos[7];
+  int i;
+  initial begin
+    for(i=0; i<7; i++)begin
+      if(i == 0)
+        cos[i] = $cos(4*PI / 16) * 2**(DATA_WIDTH-1);
+      else
+        cos[i] = $cos(i*PI / 16) * 2**(DATA_WIDTH-1);
+    end
+    memoryArray[0] = {cos[0], -1*cos[4], cos[2], cos[3]};
+    memoryArray[1] = {cos[1], cos[0], cos[3], -1*cos[5]};
+    memoryArray[2] = {cos[2], cos[3], cos[0], -1*cos[1]};
+    memoryArray[3] = {cos[3], cos[5], cos[4], cos[0]};
+    memoryArray[4] = {cos[0], -1*cos[6], -1*cos[5], -1*cos[6]};
+    memoryArray[5] = {cos[4], -1*cos[0], -1*cos[1], -1*cos[2]};
+    memoryArray[6] = {cos[5], -1*cos[1], -1*cos[0], -1*cos[4]};
+    memoryArray[7] = {cos[6], -1*cos[2], cos[6], cos[0]};
+  end
 endmodule
